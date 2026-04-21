a4ec87b65a5da79fac2bd61b3d53e0f278a405e3
Miracle Friday <afullsnack@Miracles-MacBook-Air.local>
2026-04-21 19:44:32 +0100

    refactor: Update posthog personal identification config

--- apps/website/src/integrations/posthog/provider.tsx
+++ apps/website/src/integrations/posthog/provider.tsx
@@ -9,7 +9,7 @@ if (
 ) {
   posthog.init(import.meta.env.VITE_POSTHOG_KEY || env.VITE_POSTHOG_KEY, {
     api_host: import.meta.env.VITE_POSTHOG_HOST || 'https://us.i.posthog.com',
-    person_profiles: 'identified_only',
+    person_profiles: 'always',
     capture_pageview: false,
     defaults: '2025-11-30',
   })
