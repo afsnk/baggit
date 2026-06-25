f031501003a9166f286af98b08230047bee56b97
Miracle Friday <miraclef60@gmail.com>
2026-06-25 15:10:22 +0100

    feat: Pay client for handling payment url redirects

---
+++ apps/pay/src/components/ReceiptSheet.tsx
@@ -0,0 +1,25 @@
+import { AnimatedTicket } from './receipt-confirmation-card'
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
+
+interface ReceiptSheetProps {
+  children: React.ReactNode
+}
+export const ReceiptSheet = (props: ReceiptSheetProps) => {
+  return (
+    <Sheet>
+      <SheetTrigger>{props.children}</SheetTrigger>
+      <SheetContent side="bottom" showCloseButton={true} className="h-screen">
+        <div className="w-full h-full flex flex-col items-center justify-center bg-muted">
+          <AnimatedTicket
+            ticketId="0120034399434"
+            amount={305.5}
+            date={new Date('2025-06-19T10:15:00')}
+            cardHolder="Liana80 Tudakova"
+            last4Digits="8237"
+            barcodeValue="28937261273650"
+          />
+        </div>
+      </SheetContent>
+    </Sheet>
+  )
+}
