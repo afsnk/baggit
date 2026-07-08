1dbd594bdb78fc80db139d44784b5c286cad18d0
Miracle Friday <miraclef60@gmail.com>
2026-07-08 12:25:11 +0100

    refactor(merchant): Add api keys table, and create functionality to merchant

--- apps/merchant/src/routes/_platform/developer.api-keys.tsx
+++ apps/merchant/src/routes/_platform/developer.api-keys.tsx
@@ -1,3 +1,14 @@
+import Particle from '#/components/api-table'
+import { showCreateKeysModal } from '#/components/create-key-modal'
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+  AlertAction,
+} from '#/components/ui/alert'
+import { Button } from '#/components/ui/button'
+import { authClient } from '#/lib/auth-client'
+import { useQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_platform/developer/api-keys')({
@@ -5,5 +16,47 @@ export const Route = createFileRoute('/_platform/developer/api-keys')({
 })
 
 function RouteComponent() {
-  return <div>Hello "/_platform/developer/api-keys"!</div>
+  const { data: session } = authClient.useSession()
+  const { data: apiKeys, error: _apiKeysError } = useQuery({
+    gcTime: 30_000,
+    enabled: !!session,
+    queryKey: ['getKeys'],
+    queryFn: async () => {
+      const { data, error } = await authClient.apiKey.list({
+        query: {
+          organizationId: session?.session.activeOrganizationId || '',
+        },
+      })
+
+      if (error) {
+        console.log(`Error getting keys`, { error })
+        throw error
+      }
+
+      return data
+    },
+  })
+  return (
+    <div className="grid gap-4">
+      <div className="flex items-center justify-between w-full">
+        <Alert className="bg-muted">
+          <AlertTitle>Manage API keys</AlertTitle>
+          <AlertDescription>
+            Keep these keys secret — they grant full API access. Rotate any key
+            suspected of being exposed.
+          </AlertDescription>
+          <AlertAction>
+            <Button
+              size="default"
+              variant="default"
+              onClick={showCreateKeysModal}
+            >
+              Create keys
+            </Button>
+          </AlertAction>
+        </Alert>
+      </div>
+      <Particle data={apiKeys?.apiKeys || []} />
+    </div>
+  )
 }
