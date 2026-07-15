96e8b23b796f7acb4a6736959a3af3efc42bf1dd
Miracle Friday <miraclef60@gmail.com>
2026-07-15 20:27:06 +0100

    refactor(api, merchant): Fix merchant table distortions and calculate ngn balance

--- apps/merchant/src/components/Balance.tsx
+++ apps/merchant/src/components/Balance.tsx
@@ -1,17 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
 import { Button } from "./ui/button";
 import { getBalanceOptions } from "#/lib/api-client";
+import { useIsMobile } from "#/hooks/use-mobile";
+import { PiggyBank } from "lucide-react";
 
 interface IBalanceProps { }
 export default function Balance(_props: IBalanceProps) {
   const { data, error, isLoading } = useQuery(getBalanceOptions)
+  const isMobile = useIsMobile()
   return (
     <div className='p-1 bg-muted flex items-center justify-center gap-4 rounded-lg'>
       <div className='grid items-start'>
         <span className='text-xs font-light text-muted-foreground'>Balance</span>
-        <span className='text-sm font-semibold text-primary'>{data?.totalBalance}</span>
+        <span className='text-sm font-semibold text-primary'>{data?.totalNgnBalance}</span>
       </div>
-      <Button size="sm" variant="default" disabled={isLoading || !!error}>Request payout</Button>
+      <Button
+        size={isMobile ? "icon-sm" : "sm"}
+        variant="default"
+        disabled={isLoading || !!error}>{isMobile ? <PiggyBank /> : "Request payout"}</Button>
     </div>
   )
 }
--- apps/merchant/src/components/analytics-table.tsx
+++ apps/merchant/src/components/analytics-table.tsx
@@ -42,6 +42,7 @@ import {
   TableRow,
 } from '#/components/ui/table.tsx'
 import type { Transaction } from "@baggit/api/app"
+import { DataTable } from './data-table'
 
 const getStatusColor = (status: Transaction['status']) => {
   switch (status) {
@@ -60,6 +61,7 @@ const columns: ColumnDef<Transaction>[] = [
     cell: ({ row }) => (
       <Checkbox
         aria-label="Select row"
+        className="mx-2.5"
         checked={row.getIsSelected()}
         onCheckedChange={(value) => row.toggleSelected(!!value)}
       />
@@ -189,172 +191,6 @@ export default function AnalyticsTable({ data }: { data: Array<Transaction> }) {
   })
 
   return (
-    <Frame className="w-full">
-      <Table variant="card" className="table-fixed">
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow className="hover:bg-transparent" key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                const columnSize = header.column.getSize()
-                return (
-                  <TableHead
-                    key={header.id}
-                    style={
-                      columnSize ? { width: `${columnSize}px` } : undefined
-                    }
-                  >
-                    {header.isPlaceholder ? null : header.column.getCanSort() ? (
-                      <div
-                        className="flex h-full cursor-pointer select-none items-center justify-between gap-2"
-                        onClick={header.column.getToggleSortingHandler()}
-                        onKeyDown={(e) => {
-                          if (e.key === 'Enter' || e.key === ' ') {
-                            e.preventDefault()
-                            header.column.getToggleSortingHandler()?.(e)
-                          }
-                        }}
-                        role="button"
-                        tabIndex={0}
-                      >
-                        {flexRender(
-                          header.column.columnDef.header,
-                          header.getContext(),
-                        )}
-                        {{
-                          asc: (
-                            <ChevronUpIcon
-                              aria-hidden="true"
-                              className="size-4 shrink-0 opacity-80"
-                            />
-                          ),
-                          desc: (
-                            <ChevronDownIcon
-                              aria-hidden="true"
-                              className="size-4 shrink-0 opacity-80"
-                            />
-                          ),
-                        }[header.column.getIsSorted() as string] ?? null}
-                      </div>
-                    ) : (
-                      flexRender(
-                        header.column.columnDef.header,
-                        header.getContext(),
-                      )
-                    )}
-                  </TableHead>
-                )
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                data-state={row.getIsSelected() ? 'selected' : undefined}
-                key={row.id}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell className="h-24 text-center" colSpan={columns.length}>
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-      <FrameFooter className="p-2">
-        <div className="flex items-center justify-between gap-2 max-h-[30px]">
-          {/* Results range selector */}
-          <div className="flex items-center gap-2 whitespace-nowrap">
-            <p className="text-muted-foreground text-sm">Viewing</p>
-            <Select
-              // items={Array.from({ length: table.getPageCount() }, (_, i) => {
-              //   const start = i * table.getState().pagination.pageSize + 1
-              //   const end = Math.min(
-              //     (i + 1) * table.getState().pagination.pageSize,
-              //     table.getRowCount(),
-              //   )
-              //   const pageNum = i + 1
-              //   return { label: `${start}-${end}`, value: pageNum }
-              // })}
-              onValueChange={(value) => {
-                table.setPageIndex(Number(value) - 1)
-              }}
-              value={(table.getState().pagination.pageIndex + 1).toString()}
-            >
-              <SelectTrigger
-                aria-label="Select result range"
-                className="w-fit min-w-none"
-                size="sm"
-              >
-                <SelectValue />
-              </SelectTrigger>
-              <SelectContent>
-                {Array.from({ length: table.getPageCount() }, (_, i) => {
-                  const start = i * table.getState().pagination.pageSize + 1
-                  const end = Math.min(
-                    (i + 1) * table.getState().pagination.pageSize,
-                    table.getRowCount(),
-                  )
-                  const pageNum = i + 1
-                  return (
-                    <SelectItem key={pageNum} value={pageNum.toString()}>
-                      {`${start}-${end}`}
-                    </SelectItem>
-                  )
-                })}
-              </SelectContent>
-            </Select>
-            <p className="text-muted-foreground text-sm">
-              of{' '}
-              <strong className="font-medium text-foreground">
-                {table.getRowCount()}
-              </strong>{' '}
-              results
-            </p>
-          </div>
-
-          {/* Pagination */}
-          <Pagination className="justify-end h-fit">
-            <PaginationContent>
-              <PaginationItem>
-                <PaginationPrevious
-                  className="sm:*:[svg]:hidden"
-                  render={
-                    <Button
-                      disabled={!table.getCanPreviousPage()}
-                      onClick={() => table.previousPage()}
-                      size="sm"
-                      variant="outline"
-                    />
-                  }
-                />
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationNext
-                  className="sm:*:[svg]:hidden"
-                  render={
-                    <Button
-                      disabled={!table.getCanNextPage()}
-                      onClick={() => table.nextPage()}
-                      size="sm"
-                      variant="outline"
-                    />
-                  }
-                />
-              </PaginationItem>
-            </PaginationContent>
-          </Pagination>
-        </div>
-      </FrameFooter>
-    </Frame>
+    <DataTable data={data} columns={columns} />
   )
 }
